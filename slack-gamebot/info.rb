63fa9ae539b06fb3047a4c8503dff8a75a0ddbe7
M. Oleske <moleske@users.noreply.github.com>
2016-11-01 21:51:50 -0400

    Clean Up Info
    Remove links and link to this repo

--- slack-gamebot/info.rb
+++ slack-gamebot/info.rb
@@ -2,11 +2,8 @@ module SlackGamebot
   INFO = <<-EOS
 Gamebot #{SlackGamebot::VERSION}
 
-© Daniel Doubrovkine & Contributors, MIT License
-https://twitter.com/dblockdotorg
+Started By:© Daniel Doubrovkine & Contributors, MIT License
 
-Free Service at https://www.playplay.io
-Follow Us at https://twitter.com/playplayio
-Open-Source at https://github.com/dblock/slack-gamebot
+Boston Pong Bot Fork Open-Source at https://github.com/makeit-labs/boston-pong-bot
   EOS
 end
